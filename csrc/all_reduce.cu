bb9e46a6ce878fc92a72ec54e9dcf38e8ab2f7ce
SzymonOzog <szymon.ozog@gmail.com>
2025-08-22 12:29:00 +0200

    Fix allreduce

--- csrc/all_reduce.cu
+++ csrc/all_reduce.cu
@@ -1,5 +1,8 @@
+#include "device_host_transport/nvshmem_common_transport.h"
 #include "host/nvshmem_api.h"
 #include "host/nvshmemx_api.h"
+#include <cstdint>
+#include <cstdio>
 #include <pybind11/functional.h>
 #include <torch/all.h>
 #include <ATen/cuda/CUDAContext.h>
@@ -11,33 +14,67 @@
 template <typename scalar_t>
 __global__ void all_reduce(scalar_t *destination, scalar_t* buffer, uint64_t* signal, int peer, int packet_size) 
 {
-    const uint64_t off = (blockIdx.x * blockDim.x ) * packet_size/sizeof(scalar_t);
+    const uint64_t off = (blockIdx.x * blockDim.x) * packet_size/sizeof(scalar_t);
     const uint64_t block_size = blockDim.x * packet_size;
+    const uint64_t chunk_off = (gridDim.x * blockDim.x) * packet_size/sizeof(scalar_t);
 
     for (int chunk = 0; chunk < nvshmem_n_pes() - 1; chunk++)
     {
-        nvshmemx_putmem_block(destination + off, buffer + off, block_size, peer);
+        int send_chunk = (nvshmem_n_pes() + nvshmem_my_pe() - chunk) % nvshmem_n_pes();
+        int recv_chunk = (nvshmem_n_pes() + nvshmem_my_pe() - chunk - 1) % nvshmem_n_pes();
 
-        nvshmem_fence();
-        __syncthreads();
+        nvshmem_signal_wait_until(signal + gridDim.x + blockIdx.x, NVSHMEM_CMP_GE, chunk);
 
-        constexpr uint64_t SIG_SYNC = 1;
-        if (threadIdx.x == 0)
-        {
-            nvshmemx_signal_op(signal + blockIdx.x, SIG_SYNC, NVSHMEM_SIGNAL_SET, peer);
-        }
-        nvshmem_signal_wait_until(signal + blockIdx.x, NVSHMEM_CMP_EQ, SIG_SYNC);
+
+        nvshmemx_putmem_block(destination + off, buffer + send_chunk*chunk_off + off, block_size, peer);
 
         nvshmem_fence();
         __syncthreads();
 
         if (threadIdx.x == 0)
         {
-            nvshmemx_signal_op(signal + blockIdx.x, 0, NVSHMEM_SIGNAL_SET, nvshmem_my_pe());
+            nvshmemx_signal_op(signal + blockIdx.x, 1, NVSHMEM_SIGNAL_ADD, peer);
         }
-        for (int i = 0; i < block_size/sizeof(scalar_t); i += blockDim.x)
+        nvshmem_signal_wait_until(signal + blockIdx.x, NVSHMEM_CMP_GE, chunk+1);
+
+        for (int i = threadIdx.x; i < block_size/sizeof(scalar_t); i += blockDim.x)
         {
-            buffer[i] += destination[i];
+            buffer[recv_chunk*chunk_off + off + i] += destination[off + i];
+        }
+        __syncthreads();
+        if (threadIdx.x == 0)
+        {
+            nvshmemx_signal_op(signal + gridDim.x + blockIdx.x, 1, NVSHMEM_SIGNAL_ADD, peer);
+        }
+    }
+
+    for (int chunk = 0; chunk < nvshmem_n_pes() - 1; chunk++)
+    {
+        int send_chunk = (nvshmem_n_pes() + nvshmem_my_pe() - chunk + 1) % nvshmem_n_pes();
+        int recv_chunk = (nvshmem_n_pes() + nvshmem_my_pe() - chunk) % nvshmem_n_pes();
+
+        nvshmem_signal_wait_until(signal + gridDim.x + blockIdx.x, NVSHMEM_CMP_GE, nvshmem_n_pes() - 1 + chunk);
+
+        nvshmemx_putmem_block(destination + off, buffer + send_chunk*chunk_off + off, block_size, peer);
+
+        nvshmem_fence();
+        __syncthreads();
+
+        if (threadIdx.x == 0)
+        {
+            nvshmemx_signal_op(signal + blockIdx.x, 1, NVSHMEM_SIGNAL_ADD, peer);
+        }
+        nvshmem_signal_wait_until(signal + blockIdx.x, NVSHMEM_CMP_GE, nvshmem_n_pes()+chunk);
+
+
+        for (int i = threadIdx.x; i < block_size/sizeof(scalar_t); i += blockDim.x)
+        {
+            buffer[recv_chunk*chunk_off + off + i] = destination[off + i];
+        }
+        __syncthreads();
+        if (threadIdx.x == 0 && chunk < nvshmem_n_pes() - 1)
+        {
+            nvshmemx_signal_op(signal + gridDim.x + blockIdx.x, 1, NVSHMEM_SIGNAL_ADD, peer);
         }
     }
 }
@@ -46,13 +83,14 @@ void all_reduce(torch::Tensor& buffer, int packet_size, int block_size)
 {
     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-    half *destination = (half *) nvshmem_malloc(buffer.numel() * sizeof(half));
+    half *destination = (half *) nvshmem_malloc(buffer.numel() * sizeof(half) / nvshmem_n_pes());
 
     nvshmemx_buffer_register(buffer.data_ptr(), buffer.numel() * sizeof(half));
     
-    const uint32_t grid_size = std::ceil(buffer.numel()*sizeof(half) / float(packet_size*block_size));
+    const uint32_t grid_size = std::ceil(buffer.numel()*sizeof(half) / float(packet_size*block_size*nvshmem_n_pes()));
 
-    uint64_t *signal = (uint64_t *) nvshmem_malloc(grid_size * sizeof(uint64_t));
+    uint64_t *signal = (uint64_t *) nvshmem_malloc(grid_size * 2 * sizeof(uint64_t));
+    cudaMemset(signal, 0, grid_size * 2 * sizeof(uint64_t));
     int peer = (nvshmem_my_pe()+1) % nvshmem_n_pes();
 
     all_reduce<<<grid_size, block_size, 0, stream>>>(destination,
