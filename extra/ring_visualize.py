2ff9b17314501a04fd7582d4cd766b7ebf5a92c8
SzymonOzog <szymon.ozog@gmail.com>
2025-09-01 08:03:59 +0200

    Ring visualize script

---
+++ extra/ring_visualize.py
@@ -0,0 +1,50 @@
+n_pes = 16//2
+gpus_per_node = 8//2
+
+
+for ring_id in range(3):
+    print(ring_id)
+    sends = []
+    recvs = []
+    csends = []
+    crecvs = []
+    pes = []
+    pe = 0
+    while pe not in pes:
+        pes.append(pe)
+        local_rank = pe%gpus_per_node
+        my_node = pe//gpus_per_node
+
+        ring_pos = (ring_id//2)*2 + my_node*gpus_per_node + local_rank
+
+        send_chunk = (ring_pos)%n_pes
+        recv_chunk = (ring_pos-1)%n_pes
+
+        color='\033[0m' #]
+        if local_rank == (ring_id // 2) * 2:
+            color = '\033[35m'#]
+
+            send_peer = (pe + gpus_per_node + 1) % n_pes
+            recv_peer = my_node * gpus_per_node + (gpus_per_node + local_rank - 1) % gpus_per_node
+
+        elif local_rank == (ring_id // 2) * 2 + 1:
+            color = '\033[36m'#]
+
+            send_peer = my_node * gpus_per_node + (local_rank + 1) % gpus_per_node
+            recv_peer = (n_pes + pe - gpus_per_node-1) % n_pes
+
+        else:
+            send_peer = my_node * gpus_per_node + (local_rank + 1) % gpus_per_node
+            recv_peer = my_node * gpus_per_node + (gpus_per_node + local_rank - 1) % gpus_per_node
+        warning = ""
+
+        if len(csends) and csends[-1] != recv_chunk:
+            warning = "*"
+
+        sends.append(send_peer)
+        recvs.append(recv_peer)
+        csends.append(send_chunk)
+        crecvs.append(recv_chunk)
+        print(color, pe, recv_peer, send_peer, recv_chunk, send_chunk, warning)
+        pe = send_peer
+
