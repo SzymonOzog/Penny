ac16342d4394e70558793dfaf9bd632593b2fdb8
SzymonOzog <szymon.ozog@gmail.com>
2025-09-24 19:11:04 +0200

    Cleanup

--- penny/utils.py
+++ penny/utils.py
@@ -4,69 +4,24 @@ import penny_cpp
 import os
 from typing import Optional, Union
 
-class empty_suppress:
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *_):
-        pass
-
-
-class suppress_stdout_stderr:
-    def __enter__(self):
-        self.outnull_file = open(os.devnull, 'w')
-        self.errnull_file = open(os.devnull, 'w')
-
-        self.old_stdout_fileno_undup = sys.stdout.fileno()
-        self.old_stderr_fileno_undup = sys.stderr.fileno()
-
-        self.old_stdout_fileno = os.dup(sys.stdout.fileno())
-        self.old_stderr_fileno = os.dup(sys.stderr.fileno())
-
-        self.old_stdout = sys.stdout
-        self.old_stderr = sys.stderr
-
-        os.dup2(self.outnull_file.fileno(), self.old_stdout_fileno_undup)
-        os.dup2(self.errnull_file.fileno(), self.old_stderr_fileno_undup)
-
-        sys.stdout = self.outnull_file
-        sys.stderr = self.errnull_file
-        return self
-
-    def __exit__(self, *_):
-        sys.stdout = self.old_stdout
-        sys.stderr = self.old_stderr
-
-        os.dup2(self.old_stdout_fileno, self.old_stdout_fileno_undup)
-        os.dup2(self.old_stderr_fileno, self.old_stderr_fileno_undup)
-
-        os.close(self.old_stdout_fileno)
-        os.close(self.old_stderr_fileno)
-
-        self.outnull_file.close()
-        self.errnull_file.close()
-
-
 # Adapted from: https://github.com/deepseek-ai/DeepEP/blob/main/deep_ep/utils.py
 def bench_kineto(fn, kernel_name: str, num_tests: int = 30, suppress_kineto_output: bool = False,
                  barrier_comm_profiling: bool = True):
     # Profile
-    suppress = suppress_stdout_stderr if suppress_kineto_output else empty_suppress
-    with suppress():
-        schedule = torch.profiler.schedule(wait=0, warmup=1, active=1, repeat=1)
-        with torch.profiler.profile(activities=[torch.profiler.ProfilerActivity.CUDA], schedule=schedule) as prof:
-            for i in range(2):
-                # NOTES: use a large kernel and a barrier to eliminate the unbalanced CPU launch overhead
-                if barrier_comm_profiling:
-                    lhs = torch.randn((8192, 8192), dtype=torch.float, device='cuda')
-                    rhs = torch.randn((8192, 8192), dtype=torch.float, device='cuda')
-                    lhs @ rhs
-                    dist.all_reduce(torch.ones(1, dtype=torch.float, device='cuda'))
-                for _ in range(num_tests):
-                    # Alocate big tensor to clear cache
-                    x = torch.randn((8192, 8192), dtype=torch.float, device='cuda')
-                    fn()
-                prof.step()
+    schedule = torch.profiler.schedule(wait=0, warmup=1, active=1, repeat=1)
+    with torch.profiler.profile(activities=[torch.profiler.ProfilerActivity.CUDA], schedule=schedule) as prof:
+        for i in range(2):
+            # NOTES: use a large kernel and a barrier to eliminate the unbalanced CPU launch overhead
+            if barrier_comm_profiling:
+                lhs = torch.randn((8192, 8192), dtype=torch.float, device='cuda')
+                rhs = torch.randn((8192, 8192), dtype=torch.float, device='cuda')
+                lhs @ rhs
+                dist.all_reduce(torch.ones(1, dtype=torch.float, device='cuda'))
+            for _ in range(num_tests):
+                # Alocate big tensor to clear cache
+                x = torch.randn((8192, 8192), dtype=torch.float, device='cuda')
+                fn()
+            prof.step()
 
     # Parse the profiling table
     times = []
