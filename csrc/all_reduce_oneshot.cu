48ff506eaa99fde6a7d7ca3a5a02be52039c174b
SzymonOzog <szymon.ozog@gmail.com>
2025-10-05 08:28:51 +0200

    WIP oneshot inside single block

--- csrc/all_reduce_oneshot.cu
+++ csrc/all_reduce_oneshot.cu
@@ -10,13 +10,15 @@
 #include <nvshmemx.h>
 #include "common.h"
 
-__device__ int buffer_lock = 0;
 
 template <typename scalar_t>
 __global__ void all_reduce_oneshot_kernel(scalar_t* __restrict__ destination, scalar_t* __restrict__ buffer, uint64_t* __restrict__ signal,
         const int packet_size, const int gpus_per_node, int stage)
 {
     using P = array_t<scalar_t, 16/sizeof(scalar_t)>;
+    __shared__ int buffer_lock;
+    if (threadIdx.x == 0)
+        buffer_lock = 0;
 
     const uint32_t block_size = blockDim.x * packet_size;
     const uint32_t pe_off = block_size/sizeof(scalar_t);
@@ -24,27 +26,28 @@ __global__ void all_reduce_oneshot_kernel(scalar_t* __restrict__ destination, sc
     const int pe = nvshmem_my_pe();
     const int n_pes = nvshmem_n_pes();
 
-    if (blockIdx.x == pe)
+    for (int send_pe = 0; send_pe<n_pes; send_pe++)
     {
-        for (int send_pe = 0; send_pe<n_pes; send_pe++)
-        {
-            if (send_pe == pe)
-                continue;
-            nvshmemx_putmem_signal_nbi_block(destination + pe*pe_off,
-                    buffer,
-                    block_size, signal+pe, stage, NVSHMEM_SIGNAL_SET, send_pe);
-        }
-        return;
+        if (send_pe == pe) continue;
+
+        nvshmemx_putmem_signal_nbi_block(destination + pe*pe_off,
+                buffer,
+                block_size, signal+pe, stage, NVSHMEM_SIGNAL_SET, send_pe);
     }
-    int recv_pe = blockIdx.x;
-    if (threadIdx.x == 0)
+    nvshmem_quiet();
+    __syncthreads();
+    int warp_id = threadIdx.x/32;
+    int lane_id = threadIdx.x%32;
+    int recv_pe = warp_id;
+
+    if (lane_id == 0)
     {
         nvshmem_signal_wait_until(signal+recv_pe, NVSHMEM_CMP_EQ, stage);
         while (atomicCAS(&buffer_lock, 0, 1) != 0) {/*wait*/}
     }
 
-    __syncthreads();
-    for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
+    __syncwarp();
+    for (int i = lane_id; i < block_size/(sizeof(P)); i += 32)
     {
         P buf = reinterpret_cast<P*>(buffer)[i];
         P dst = reinterpret_cast<P*>(destination + recv_pe*pe_off)[i];
@@ -53,18 +56,19 @@ __global__ void all_reduce_oneshot_kernel(scalar_t* __restrict__ destination, sc
             res.data[j] = float(buf.data[j]) + float(dst.data[j]);
         reinterpret_cast<P*>(buffer)[i] = res;
     }
-    __syncthreads();
-    if (threadIdx.x == 0) {
-        atomicExch(&buffer_lock, 0); // Release lock
+    __syncwarp();
+    if (lane_id == 0) 
+    {
+        atomicExch(&buffer_lock, 0);
     }
 }
 
 AllReduceOneShot::AllReduceOneShot(half* _buffer, int numel, int packet_size, int block_size, int nnodes, int routes, cudaStream_t stream)
-    : AllReduce(_buffer, numel*nvshmem_n_pes(), packet_size, block_size, nnodes,
+    : AllReduce(_buffer, numel*nvshmem_n_pes(), packet_size, 32*nvshmem_n_pes(), nnodes,
             nvshmem_n_pes(), stream)
 {
     assert(packet_size*block_size  == numel * sizeof(half));
-    grid_dim.x = nvshmem_n_pes();
+    grid_dim.x = 1;
     grid_dim.y = 1;
 }
 void AllReduceOneShot::run(cudaStream_t stream)
