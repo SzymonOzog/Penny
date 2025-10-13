916f966e2b65fa8e549d23081e6201edd24a0d20
SzymonOzog <szymon.ozog@gmail.com>
2025-10-13 20:24:16 +0200

    Twoshot with multiblock reduction

--- csrc/all_reduce_twoshot.cu
+++ csrc/all_reduce_twoshot.cu
@@ -15,7 +15,7 @@
 
 __device__ int buffer_lock_[4] = {0};
 
-template <typename scalar_t>
+template <typename scalar_t, int N_PES=8>
 __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, scalar_t*  buffer, uint64_t* signal,
         const int packet_size, const int gpus_per_node, int stage)
 {
@@ -28,45 +28,8 @@ __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, sc
 
     const uint32_t pe_off = block_size/sizeof(scalar_t);
 
-    auto reduce = [&](scalar_t* s1_p, scalar_t* s2_p, scalar_t* dst)
-    {
-        for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
-        {
-            P src1 = reinterpret_cast<P*>(s1_p)[i];
-            P src2 = reinterpret_cast<P*>(s2_p)[i];
-            P res;
-            for (int j = 0; j < P::size; j++)
-                res.data[j] = float(src1.data[j]) + float(src2.data[j]);
-            reinterpret_cast<P*>(dst)[i] = res;
-        }
-    };
     uint32_t write_chunk = blockIdx.x;
-    //Makes reduciton easier
-    if (blockIdx.x == 0)
-        write_chunk = pe;
-    if( blockIdx.x == pe)
-        write_chunk = 0;
 
-    auto wait_send_write = [&]()
-    {
-        if (threadIdx.x == 0)
-        {
-            nvshmem_signal_wait_until(signal+n_pes+pe, NVSHMEM_CMP_EQ, stage);
-        }
-        __syncthreads();
-        __threadfence();
-        nvshmemx_putmem_signal_nbi_block(destination + (n_pes+pe)*pe_off,
-                buffer + pe*pe_off,
-                block_size, signal+n_pes+pe, stage, NVSHMEM_SIGNAL_SET, write_chunk);
-        if (threadIdx.x == 0)
-            nvshmem_signal_wait_until(signal+n_pes+write_chunk, NVSHMEM_CMP_EQ, stage);
-        __syncthreads();
-        for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
-        {
-            reinterpret_cast<P*>(buffer + write_chunk*pe_off)[i] =
-                reinterpret_cast<P*>(destination + (n_pes+write_chunk)*pe_off)[i];
-        }
-    };
 
     if (write_chunk != pe)
     {
@@ -74,71 +37,58 @@ __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, sc
                     buffer + write_chunk*pe_off,
                     block_size, signal+pe, stage, NVSHMEM_SIGNAL_SET, write_chunk);
     }
-    int off = 4;
-    if (blockIdx.x >= off)
+    for(int tid = 0; tid<N_PES; tid++)
     {
-        wait_send_write();
-        return;
-    }
-
-    int recv_pe0 = blockIdx.x;
-    int recv_pe1 = blockIdx.x + off;
-
-    if (threadIdx.x == 0 && recv_pe0 != pe)
-    {
-        nvshmem_signal_wait_until(signal+recv_pe0, NVSHMEM_CMP_EQ, stage);
-    }
-    if (threadIdx.x == 1 && recv_pe1 != pe)
-    {
-        nvshmem_signal_wait_until(signal+recv_pe1, NVSHMEM_CMP_EQ, stage);
+        if (threadIdx.x == tid && tid != pe)
+        {
+            nvshmem_signal_wait_until(signal+tid, NVSHMEM_CMP_EQ, stage);
+        }
     }
 
     __syncthreads();
-    reduce(recv_pe0 == pe ? buffer + pe*pe_off : destination + recv_pe0*pe_off,
-            recv_pe1 == pe ? buffer + pe*pe_off : destination + recv_pe1*pe_off,
-            destination + recv_pe0*pe_off);
-    __syncthreads();
+    const uint32_t write_size = block_size/N_PES;
+    const uint32_t block_off = blockIdx.x*write_size/sizeof(scalar_t);
 
-    off /= 2;
-    if (blockIdx.x >= off)
+    for (int i = threadIdx.x; i < write_size/(sizeof(P)); i += blockDim.x)
     {
-        if (threadIdx.x == 0) { atomicAdd(&buffer_lock_[blockIdx.x], 1); }
-        wait_send_write();
-        return;
+        P res = reinterpret_cast<P*>(buffer + pe*pe_off + block_off)[i];
+        for (int recv_pe = 0; recv_pe < N_PES; recv_pe++)
+        {
+            if(recv_pe == pe)
+                continue;
+            P src = reinterpret_cast<P*>(destination + recv_pe*pe_off + block_off)[i];
+            for (int j = 0; j < P::size; j++)
+            {
+                res.data[j] += float(src.data[j]);
+            }
+        }
+        reinterpret_cast<P*>(buffer + pe * pe_off + block_off)[i] = res;
     }
-    if(threadIdx.x == 0)
-    {
-        while(atomicCAS(&buffer_lock_[blockIdx.x+off], 1, 0) != 1) { }
-    }
-
-    __syncthreads();
-    recv_pe1 = blockIdx.x + off;
-    reduce(destination + recv_pe0*pe_off,
-            destination + recv_pe1*pe_off,
-            destination + recv_pe0*pe_off);
-    __syncthreads();
-
-    off /= 2;
-    if (blockIdx.x >= off)
-    {
-        if (threadIdx.x == 0) { atomicAdd(&buffer_lock_[blockIdx.x], 1); }
-        wait_send_write();
-        return;
-    }
-    if(threadIdx.x == 0)
-    {
-        while(atomicCAS(&buffer_lock_[blockIdx.x+off], 1, 0) != 1) {}
-        nvshmem_fence();
-    }
-    __syncthreads();
-    recv_pe1 = blockIdx.x + off;
-    reduce(destination + recv_pe0*pe_off,
-            destination + recv_pe1*pe_off,
-            buffer + write_chunk*pe_off);
     __syncthreads();
     if (threadIdx.x == 0)
     {
-        nvshmemx_signal_op(signal+n_pes+write_chunk, stage, NVSHMEM_SIGNAL_SET, pe);
+        nvshmemx_signal_op(signal+n_pes+pe, 1, NVSHMEM_SIGNAL_ADD, pe);
+    }
+    if (write_chunk == pe)
+    {
+        return;
+    }
+    if (threadIdx.x == 0)
+    {
+        nvshmem_signal_wait_until(signal+n_pes+pe, NVSHMEM_CMP_EQ, stage*N_PES);
+    }
+    __syncthreads();
+    __threadfence_system();
+    nvshmemx_putmem_signal_nbi_block(destination + (n_pes+pe)*pe_off,
+            buffer + pe*pe_off,
+            block_size, signal+n_pes+pe, stage, NVSHMEM_SIGNAL_SET, write_chunk);
+    if (threadIdx.x == 0)
+        nvshmem_signal_wait_until(signal+n_pes+write_chunk, NVSHMEM_CMP_EQ, stage);
+    __syncthreads();
+    for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
+    {
+        reinterpret_cast<P*>(buffer + write_chunk*pe_off)[i] =
+            reinterpret_cast<P*>(destination + (n_pes+write_chunk)*pe_off)[i];
     }
 }
 
@@ -160,5 +110,5 @@ void AllReduceTwoShot::run(cudaStream_t stream)
             gpus_per_node,
             stage
             );
-    stage+=2;
+    stage+=1;
 }
