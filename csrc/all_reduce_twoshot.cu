a072ff79bedfa4f20ba5aff34c91083396db8622
SzymonOzog <szymon.ozog@gmail.com>
2025-10-15 20:16:08 +0200

    Half parallel two stage

--- csrc/all_reduce_twoshot.cu
+++ csrc/all_reduce_twoshot.cu
@@ -99,43 +99,36 @@ __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, sc
             destination + recv_pe0*pe_off);
     __syncthreads();
 
-    off /= 2;
+    off /= 4;
     if (blockIdx.x >= off)
     {
         if (threadIdx.x == 0) { atomicAdd(&buffer_lock_[blockIdx.x], 1); }
         wait_send_write();
         return;
     }
-    if(threadIdx.x == 0)
+    if(threadIdx.x < 3)
     {
-        while(atomicCAS(&buffer_lock_[blockIdx.x+off], 1, 0) != 1) { }
-    }
-
-    __syncthreads();
-    recv_pe1 = blockIdx.x + off;
-    reduce(destination + recv_pe0*pe_off,
-            destination + recv_pe1*pe_off,
-            destination + recv_pe0*pe_off);
-    __syncthreads();
-
-    off /= 2;
-    if (blockIdx.x >= off)
-    {
-        if (threadIdx.x == 0) { atomicAdd(&buffer_lock_[blockIdx.x], 1); }
-        wait_send_write();
-        return;
-    }
-    if(threadIdx.x == 0)
-    {
-        while(atomicCAS(&buffer_lock_[blockIdx.x+off], 1, 0) != 1) {}
+        while(atomicCAS(&buffer_lock_[1+threadIdx.x], 1, 0) != 1) { }
         nvshmem_fence();
     }
+
     __syncthreads();
-    recv_pe1 = blockIdx.x + off;
-    reduce(destination + recv_pe0*pe_off,
-            destination + recv_pe1*pe_off,
-            buffer + write_chunk*pe_off);
+    for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
+    {
+        P res = reinterpret_cast<P*>(destination + recv_pe0*pe_off)[i];
+        for (int recv_pe = 1; recv_pe < 4; recv_pe++)
+        {
+            P src = reinterpret_cast<P*>(destination + recv_pe*pe_off)[i];
+            for (int j = 0; j < P::size; j++)
+            {
+                res.data[j] += float(src.data[j]);
+            }
+        }
+        reinterpret_cast<P*>(buffer + pe * pe_off)[i] = res;
+    }
+
     __syncthreads();
+
     if (threadIdx.x == 0)
     {
         nvshmemx_signal_op(signal+n_pes+write_chunk, stage, NVSHMEM_SIGNAL_SET, pe);
