a386d9a7143f2c36f9fd2e04aa0d2a85b4628ef5
SzymonOzog <szymon.ozog@gmail.com>
2025-10-18 08:24:47 +0200

    Half parallel multiblock allreduce

--- csrc/all_reduce_twoshot.cu
+++ csrc/all_reduce_twoshot.cu
@@ -28,18 +28,6 @@ __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, sc
 
     const uint32_t pe_off = block_size/sizeof(scalar_t);
 
-    auto reduce = [&](scalar_t* s1_p, scalar_t* s2_p, scalar_t* dst)
-    {
-        for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
-        {
-            P src1 = reinterpret_cast<P*>(s1_p)[i];
-            P src2 = reinterpret_cast<P*>(s2_p)[i];
-            P res;
-            for (int j = 0; j < P::size; j++)
-                res.data[j] = float(src1.data[j]) + float(src2.data[j]);
-            reinterpret_cast<P*>(dst)[i] = res;
-        }
-    };
     uint32_t write_chunk = blockIdx.x;
     //Makes reduciton easier
     if (blockIdx.x == 0)
@@ -68,21 +56,16 @@ __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, sc
         }
     };
 
-    if (write_chunk != pe)
+    if (write_chunk != pe && blockIdx.y == 0)
     {
             nvshmemx_putmem_signal_nbi_block(destination + pe*pe_off,
                     buffer + write_chunk*pe_off,
                     block_size, signal+pe, stage, NVSHMEM_SIGNAL_SET, write_chunk);
     }
     int off = 4;
-    if (blockIdx.x >= off)
-    {
-        wait_send_write();
-        return;
-    }
 
-    int recv_pe0 = blockIdx.x;
-    int recv_pe1 = blockIdx.x + off;
+    int recv_pe0 = blockIdx.x%off;
+    int recv_pe1 = recv_pe0 + off;
 
     if (threadIdx.x == 0 && recv_pe0 != pe)
     {
@@ -94,44 +77,93 @@ __global__ void all_reduce_twoshot_kernel(scalar_t* __restrict__ destination, sc
     }
 
     __syncthreads();
-    reduce(recv_pe0 == pe ? buffer + pe*pe_off : destination + recv_pe0*pe_off,
-            recv_pe1 == pe ? buffer + pe*pe_off : destination + recv_pe1*pe_off,
-            destination + recv_pe0*pe_off);
+    const int blocks = gridDim.y * gridDim.x/off;
+    {
+        const uint32_t reduce_size = block_size/blocks;
+        const uint32_t reduce_off = (blockIdx.y * blocks/gridDim.y + blockIdx.x/off) * reduce_size/sizeof(scalar_t);
+
+        scalar_t* s1_p = recv_pe0 == pe ? buffer + pe*pe_off : destination + recv_pe0*pe_off;
+        scalar_t* s2_p =  recv_pe1 == pe ? buffer + pe*pe_off : destination + recv_pe1*pe_off;
+        scalar_t* dst = destination + recv_pe0*pe_off;
+
+        // if(pe == 0 && blockIdx.x%4 == 1 && threadIdx.x == 0)
+        //     printf("reducing %d, %d, %d, %d,block  %d, %d \n",
+        //             reduce_size, reduce_off, int(s1_p[reduce_off]), int(s2_p[reduce_off]), blockIdx.x, blockIdx.y);
+
+        for (int i = threadIdx.x; i < reduce_size/(sizeof(P)); i += blockDim.x)
+        {
+            P src1 = reinterpret_cast<P*>(s1_p + reduce_off)[i];
+            P src2 = reinterpret_cast<P*>(s2_p + reduce_off)[i];
+            P res;
+            for (int j = 0; j < P::size; j++)
+                res.data[j] = float(src1.data[j]) + float(src2.data[j]);
+            reinterpret_cast<P*>(dst + reduce_off)[i] = res;
+        }
+    }
     __syncthreads();
 
-    off /= 4;
-    if (blockIdx.x >= off)
+    if (threadIdx.x == 0) { atomicAdd(&buffer_lock_[blockIdx.x%off], 1); }
+
+    if(threadIdx.x < 4)
     {
-        if (threadIdx.x == 0) { atomicAdd(&buffer_lock_[blockIdx.x], 1); }
-        wait_send_write();
-        return;
-    }
-    if(threadIdx.x < 3)
-    {
-        while(atomicCAS(&buffer_lock_[1+threadIdx.x], 1, 0) != 1) { }
+        while(atomicAdd(&buffer_lock_[threadIdx.x], 0) != blocks) { }
         nvshmem_fence();
     }
 
     __syncthreads();
-    for (int i = threadIdx.x; i < block_size/(sizeof(P)); i += blockDim.x)
+    const uint32_t reduce_size = block_size/(n_pes*gridDim.y);
+    const uint32_t reduce_off = (blockIdx.y*gridDim.x + blockIdx.x)*reduce_size/sizeof(scalar_t);
+    for (int i = threadIdx.x; i < reduce_size/(sizeof(P)); i += blockDim.x)
     {
-        P res = reinterpret_cast<P*>(destination + recv_pe0*pe_off)[i];
+        P res = reinterpret_cast<P*>(destination + recv_pe0*pe_off + reduce_off)[i];
         for (int recv_pe = 1; recv_pe < 4; recv_pe++)
         {
-            P src = reinterpret_cast<P*>(destination + recv_pe*pe_off)[i];
+            P src = reinterpret_cast<P*>(destination + recv_pe*pe_off + reduce_off)[i];
             for (int j = 0; j < P::size; j++)
             {
                 res.data[j] += float(src.data[j]);
             }
         }
-        reinterpret_cast<P*>(buffer + pe * pe_off)[i] = res;
+        reinterpret_cast<P*>(buffer + pe * pe_off + reduce_off)[i] = res;
     }
 
     __syncthreads();
-
     if (threadIdx.x == 0)
     {
-        nvshmemx_signal_op(signal+n_pes+write_chunk, stage, NVSHMEM_SIGNAL_SET, pe);
+        nvshmemx_signal_op(signal+n_pes+pe, 1, NVSHMEM_SIGNAL_ADD, pe);
+    }
+    if (threadIdx.x == 0)
+    {
+         
+        nvshmem_signal_wait_until(signal+n_pes+pe, NVSHMEM_CMP_EQ, stage*nvshmem_n_pes()*gridDim.y);
+    }
+    __syncthreads();
+    __threadfence_system();
+    if (write_chunk == pe)
+    {
+        reinterpret_cast<int4*>(buffer_lock_)[0] = make_int4(0,0,0,0);
+        return;
+    }
+
+
+    if(blockIdx.y == 0)
+    {
+        nvshmemx_putmem_signal_nbi_block(destination + (n_pes+pe)*pe_off,
+                buffer + pe*pe_off,
+                block_size, signal+n_pes+pe, stage, NVSHMEM_SIGNAL_SET, write_chunk);
+    }
+
+    if (threadIdx.x == 0)
+        nvshmem_signal_wait_until(signal+n_pes+write_chunk, NVSHMEM_CMP_EQ, stage);
+    __syncthreads();
+
+    const uint32_t write_size = block_size/gridDim.y;
+    const uint32_t write_off = (blockIdx.y*write_size)/sizeof(scalar_t);
+
+    for (int i = threadIdx.x; i < write_size/(sizeof(P)); i += blockDim.x)
+    {
+        reinterpret_cast<P*>(buffer + write_chunk*pe_off + write_off)[i] =
+            reinterpret_cast<P*>(destination + (n_pes+write_chunk)*pe_off + write_off)[i];
     }
 }
 
@@ -140,8 +172,10 @@ AllReduceTwoShot::AllReduceTwoShot(half* _buffer, int numel, int packet_size, in
             nvshmem_n_pes()*2, stream)
 {
     assert(packet_size*block_size  == numel * sizeof(half) / nvshmem_n_pes());
+    assert(block_size*packet_size/(nvshmem_n_pes()*routes) > 0);
+    assert(((block_size*packet_size)/(nvshmem_n_pes()*routes))%16 == 0);
     grid_dim.x = nvshmem_n_pes();
-    grid_dim.y = 1;
+    grid_dim.y = routes;
 }
 void AllReduceTwoShot::run(cudaStream_t stream)
 {
@@ -153,5 +187,5 @@ void AllReduceTwoShot::run(cudaStream_t stream)
             gpus_per_node,
             stage
             );
-    stage+=2;
+    stage+=1;
 }
